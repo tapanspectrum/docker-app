c0107a14b4ad07b72be05b774eb20d85b133a1cd
Tapan Acharjee <tapanspectrum@gmail.com>
2026-01-01 18:52:35 +0530

    code updated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
+  console.log("Request received at /");
   res.send("Hello from Node + Docker + Jenkins!");
 });
 
